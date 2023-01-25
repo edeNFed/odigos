14e24978b8053a8270e5c8bfd07d0c61b3f22dcc
Eden Federman <eden@keyval.dev>
2023-01-25 14:18:47 -0800

    kubelet communications poc

--- device-plugin/go.mod
+++ device-plugin/go.mod
@@ -4,18 +4,18 @@ go 1.18
 
 require (
 	github.com/kubevirt/device-plugin-manager v1.19.4
-	k8s.io/kubelet v0.19.2
+	k8s.io/kubelet v0.26.1
 )
 
 require (
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
-	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
-	github.com/golang/protobuf v1.4.2 // indirect
-	golang.org/x/net v0.0.0-20200904194848-62affa334b73 // indirect
-	golang.org/x/sys v0.0.0-20200917073148-efd3b9a0ff20 // indirect
-	golang.org/x/text v0.3.3 // indirect
-	google.golang.org/genproto v0.0.0-20200916143405-f6a2fa72f0c4 // indirect
-	google.golang.org/grpc v1.32.0 // indirect
-	google.golang.org/protobuf v1.25.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.3.1-0.20221206200815-1e63c2f08a10 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+	google.golang.org/genproto v0.0.0-20220502173005-c8bf987b8c21 // indirect
+	google.golang.org/grpc v1.49.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
--- device-plugin/pkg/plugin.go
+++ device-plugin/pkg/plugin.go
@@ -21,8 +21,8 @@ func NewInstrumentationPlugin() dpm.PluginInterface {
 func (p *plugin) GetDevicePluginOptions(ctx context.Context, empty *v1beta1.Empty) (*v1beta1.DevicePluginOptions, error) {
 	fmt.Println("GetDevicePluginOptions")
 	return &v1beta1.DevicePluginOptions{
-		PreStartRequired:                false,
-		GetPreferredAllocationAvailable: false,
+		PreStartRequired:                true,
+		GetPreferredAllocationAvailable: true,
 	}, nil
 }
 
@@ -31,6 +31,10 @@ func (p *plugin) ListAndWatch(empty *v1beta1.Empty, server v1beta1.DevicePlugin_
 		Devices: []*v1beta1.Device{
 			{
 				ID:     "instrumentation-device",
+				Health: v1beta1.Healthy,
+			},
+			{
+				ID:     "instrumentation-device2",
 				Health: v1beta1.Healthy,
 			},
 		},
@@ -46,17 +50,25 @@ func (p *plugin) Stop() error {
 }
 
 func (p *plugin) GetPreferredAllocation(ctx context.Context, request *v1beta1.PreferredAllocationRequest) (*v1beta1.PreferredAllocationResponse, error) {
+	log.Printf("GetPreferredAllocation request: %v", request)
 	return &v1beta1.PreferredAllocationResponse{}, nil
 }
 
 func (p *plugin) Allocate(ctx context.Context, request *v1beta1.AllocateRequest) (*v1beta1.AllocateResponse, error) {
 	log.Printf("Allocate request: %v", request)
+
+	err := getPodResources()
+	if err != nil {
+		return nil, err
+	}
+
 	res := &v1beta1.AllocateResponse{}
 
 	for range request.ContainerRequests {
 		res.ContainerResponses = append(res.ContainerResponses, &v1beta1.ContainerAllocateResponse{
 			Envs: map[string]string{
 				"ODIGOS_INSTRUMENTATION": "true",
+				"EDEN_TEST":              "${HOSTNAME}",
 			},
 			Mounts: []*v1beta1.Mount{
 				{
@@ -71,5 +83,6 @@ func (p *plugin) Allocate(ctx context.Context, request *v1beta1.AllocateRequest)
 }
 
 func (p *plugin) PreStartContainer(ctx context.Context, request *v1beta1.PreStartContainerRequest) (*v1beta1.PreStartContainerResponse, error) {
+	log.Printf("PreStartContainer request: %v", request)
 	return &v1beta1.PreStartContainerResponse{}, nil
 }
