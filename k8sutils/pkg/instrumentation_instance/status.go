d72c0204681d3bfc4fe99e102161379a4eac21e6
Eden Federman <eden@keyval.dev>
2024-10-06 16:35:16 +0300

    backporttest

--- k8sutils/pkg/instrumentation_instance/status.go
+++ k8sutils/pkg/instrumentation_instance/status.go
@@ -54,7 +54,6 @@ func updateInstrumentationInstanceStatus(status odigosv1.InstrumentationInstance
 }
 
 func InstrumentationInstanceName(ownerName string, pid int) string {
-
 	return fmt.Sprintf("%s-%d", ownerName, pid)
 }
 
